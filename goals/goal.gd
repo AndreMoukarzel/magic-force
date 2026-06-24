c9b8c263b2249407cd5ac400f1ebe1a01ebcc0b2
Andre Ferrari Moukarzel <andremoukarzel@gmail.com>
2026-06-24 14:53:04 -0300

    Adds collisions to map and makes Fist not change color for other players

--- goals/goal.gd
+++ goals/goal.gd
@@ -3,14 +3,21 @@ extends Node3D
 signal goal_scored
 
 
-func _on_area_3d_body_entered(body: Node3D) -> void:
-	var forward = -global_transform.basis.z.normalized()
-	var entry_direction: Vector3 = body.global_position.direction_to(self.global_position).normalized()
-	var dot_val = forward.dot(entry_direction)
+func _ready() -> void:
+	$Area3D.connect("body_entered", _on_area_3d_body_entered)
+	$Area3D.set_collision_mask_value(4, true)
+	$Area3D.set_collision_mask_value(1, false)
+	$Area3D.monitorable = false
+
+
+func _on_area_3d_body_entered(_body: Node3D) -> void:
+	#var forward = -global_transform.basis.z.normalized()
+	#var entry_direction: Vector3 = body.global_position.direction_to(self.global_position).normalized()
+	#var dot_val = forward.dot(entry_direction)
 	
-	if dot_val > 0: # Ball entered from the front
-		emit_signal("goal_scored")
-		deactivate()
+	#if dot_val > 0: # Ball entered from the front
+	emit_signal("goal_scored")
+	deactivate()
 
 
 func deactivate() -> void:
