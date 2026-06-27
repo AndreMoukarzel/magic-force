37bdad9d6c745ad51de3ccd2a91a6bff2d03bba1
Andre Ferrari Moukarzel <andremoukarzel@gmail.com>
2026-06-27 12:05:00 -0300

    Adds visuals to starting menu

--- lobby/steam_hud.gd
+++ lobby/steam_hud.gd
@@ -1,5 +1,8 @@
 extends Control
 
+
+const LOBBY_BTN_SCN := preload("res://lobby/steam_lobby_button.tscn")
+
 var IS_JOINING: bool = false
 
 
@@ -28,19 +31,16 @@ func _on_list_lobbies_pressed() -> void:
 func _on_lobby_match_list(lobbies: Array) -> void:
 	for lobby_child in %LoadedLobbies.get_children():
 		lobby_child.queue_free()
-
+	
 	for lobby in lobbies:
 		var lobby_name: String = Steam.getLobbyData(lobby, "name")
 		
 		if lobby_name == Global.STEAM_LOBBY_NAME:
-			var lobby_mode: String = Steam.getLobbyData(lobby, "mode")
-			var lobby_button: Button = Button.new()
-			#lobby_button.toggle_mode = true
-			lobby_button.set_text(lobby_name + " | " + lobby_mode)
-			lobby_button.set_size(Vector2(100, 30))
-			lobby_button.add_theme_font_size_override("font_size", 8)
+			#var lobby_mode: String = Steam.getLobbyData(lobby, "mode")
+			var lobby_button: TextureButton = LOBBY_BTN_SCN.instantiate()
+			
 			lobby_button.set_name("lobby_%s" % lobby)
-			lobby_button.alignment = HORIZONTAL_ALIGNMENT_LEFT
+			lobby_button.get_node("Label").set_text(str(lobby))
 			lobby_button.pressed.connect(join_lobby.bind(lobby))
 			
 			%LoadedLobbies.add_child(lobby_button)
