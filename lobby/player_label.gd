223e618ba1670e579ccc810b9142351fb2060221
Andre Ferrari Moukarzel <andremoukarzel@gmail.com>
2026-07-01 13:19:42 -0300

    Basic visuals for Team selectionn

--- lobby/player_label.gd
+++ lobby/player_label.gd
@@ -1,12 +1,12 @@
-extends HFlowContainer
+extends Control
 class_name PlayerLabel
 
 
 func set_player_name(text: String) -> void:
-	$Name.text = text
+	%Name.text = text
 
 func toggle_player_ready() -> void:
-	$Ready.button_pressed = not $Ready.button_pressed
+	%Ready.button_pressed = not %Ready.button_pressed
 
 func is_ready() -> bool:
-	return $Ready.button_pressed
+	return %Ready.button_pressed
